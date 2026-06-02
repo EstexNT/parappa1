6ca0d69bbf6c22380a6fb76e74562387d445e666
EstexNT <estex64@gmail.com>
2026-06-02 22:52:03 +0300

    Fix splat version, match prmemory

--- src/prr.bin/prmemory.c
+++ src/prr.bin/prmemory.c
@@ -1,20 +1,111 @@
-#include "common.h"
+#include "prmemory.h"
+// todo: move this to header
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemoryInit);
+#define MEMORY_DATA_SIZE 0x128000
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemoryClear);
+u8 *memoryPosEndBase = NULL;
+s32 memoryPosIdx = 0;
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemoryAlloc);
+extern u8 *memoryPosBase;
+extern u8 *memoryPosPtr;
+extern u8 *memoryPosPtrEnd;
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemoryFree);
+// u8 *memoryPos[MEMORY_POS_SIZE];
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemoryEndAlloc);
+extern u8 memoryData[MEMORY_DATA_SIZE];
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemoryGetInfo);
+void MemoryInit(void) {
+    memoryPosPtr = memoryPosBase = memoryData;
+    memoryPosPtrEnd = &memoryPosBase[MEMORY_DATA_SIZE];
+    memoryPosIdx = 0;
+    memoryPos[memoryPosIdx] = memoryPosPtr;
+    memoryPos[0] = NULL;
+}
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemorySetPos);
+void MemoryClear(void) {
+    s32 i;
+  
+    for (i = 0; i < MEMORY_POS_SIZE; i++) {
+        memoryPos[i] = NULL;
+    }
+    MemoryInit();
+}
 
-INCLUDE_ASM("asm/prr.bin/nonmatchings/prmemory", MemoryGetPos);
+#define MEMALIGNUP(n, al) ((((n) + ((al) - 1)) / (al)) * (al))
+
+void *MemoryAlloc(s32 size) {
+    u8 *ptr;
+    void *ret;
+    s32 alsize;
+
+    if (memoryPosIdx >= MEMORY_POS_SIZE) {
+        return NULL;
+    }
+
+    if ((size <= 0) || (size >= MEMORY_DATA_SIZE)) {
+        return NULL;
+    }
+
+    alsize = MEMALIGNUP(size, 8);
+    ptr = &memoryPosPtr[alsize];
+    if (ptr >= memoryPosPtrEnd) {
+        return NULL;
+    }
+
+    memoryPosIdx++;
+    memoryPos[memoryPosIdx] = memoryPosPtr;
+    ret = memoryPosPtr;
+    memoryPosPtr = ptr;
+    return ret;
+}
+
+void MemoryFree(void) {
+    memoryPosPtr = memoryPos[memoryPosIdx];
+    memoryPos[memoryPosIdx] = NULL;
+    memoryPosIdx--;
+}
+
+void *MemoryEndAlloc(s32 size) {
+    if (&memoryPosPtr[MEMALIGNUP(size, 8)] >= memoryPosPtrEnd) {
+        return NULL;
+        return; // crying
+    }
+
+    memoryPosEndBase = (memoryPosEndBase < &memoryPosPtrEnd[-MEMALIGNUP(size, 8)]) ? &memoryPosPtrEnd[-MEMALIGNUP(size, 8)] : memoryPosEndBase;
+    return &memoryPosPtrEnd[-MEMALIGNUP(size, 8)];
+}
+
+void MemoryGetInfo(MEM_INFO *info) {
+    info->base = memoryPosBase;
+    info->ptrend = memoryPosPtrEnd;
+    info->ptr = memoryPosPtr;
+    info->endbase = memoryPosEndBase;
+    info->left = memoryPosPtrEnd - memoryPosPtr;
+    info->used = memoryPosPtr - memoryPosBase;
+}
+
+u32 MemorySetPos(u8 *ptr, u32 pos, s32 size) {
+    u8 *ptrend = ptr + size;
+    if (ptrend >= memoryPosPtrEnd) {
+        return 0;
+    }
+
+    memoryPos[pos] = ptr;
+    memoryPosIdx = pos;
+    memoryPosPtr = ptrend;
+    return 1;
+}
+
+u32 MemoryGetPos(u8 *ptr) {
+    s32 i;
+
+    for (i = 1; !(i > memoryPosIdx); i++) {
+        if (memoryPos[i] == ptr) {
+            return i;
+        }
+    }
+    return 0;
+}
 
 void MemoryZero(u8 *ptr, s32 size) {
     for (size; size > 0; size--) {
---
+++ src/prr.bin/prmemory.h
@@ -0,0 +1,30 @@
+#ifndef _PRMEMORY_H_
+#define _PRMEMORY_H_
+
+#include "common.h"
+
+#define MEMORY_POS_SIZE 0x200
+
+typedef struct MEM_INFO {
+    u8 *base;
+    u8 *ptrend;
+    u8 *ptr;
+    u8 *endbase;
+    u32 used; // memory used 
+    u32 left; // memory left
+} MEM_INFO;
+
+extern s32 memoryPosIdx;
+extern u8 *memoryPos[MEMORY_POS_SIZE];
+
+void MemoryInit(void);
+void MemoryClear(void);
+void *MemoryAlloc(s32 size);
+void MemoryFree(void);
+void *MemoryEndAlloc(s32 size);
+void MemoryGetInfo(MEM_INFO *info);
+u32 MemorySetPos(u8 *ptr, u32 pos, s32 size);
+u32 MemoryGetPos(u8 *ptr);
+void MemoryZero(u8 *ptr, s32 size);
+
+#endif
